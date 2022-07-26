c9edd1812e88db07b7f835c7be1bbf40d619accf
unknown <hazine505@gmail.com>
2022-07-27 02:59:32 +0300

    Заданы 2 массива, сделан метод печати массивов

--- TaskXSolution/Program.cs
+++ TaskXSolution/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿string[] firstStringMassive = { "num", "345", "34", "3435", "35", "third", "-35", "nums" };
+string[] secondStringMassive = new string[firstStringMassive.Length];
+
+
+PrintArray(secondStringMassive);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
