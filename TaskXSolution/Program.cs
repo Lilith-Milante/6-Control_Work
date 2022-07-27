5e587d95ee73c4ac9131b5490e8c19a5443c4570
unknown <hazine505@gmail.com>
2022-07-27 03:02:32 +0300

    Прописан основной метод, дописан сам код с применением метода

--- TaskXSolution/Program.cs
+++ TaskXSolution/Program.cs
@@ -1,8 +1,21 @@
 ﻿string[] firstStringMassive = { "num", "345", "34", "3435", "35", "third", "-35", "nums" };
 string[] secondStringMassive = new string[firstStringMassive.Length];
 
-
+SelectWithThreeSym(firstStringMassive, secondStringMassive);
 PrintArray(secondStringMassive);
+
+void SelectWithThreeSym(string[] firstStringMassive, string[] secondStringMassive)
+{
+    int count = 0;
+    for (int i = 0; i < firstStringMassive.Length; i++)
+    {
+        if (firstStringMassive[i].Length <= 3)
+        {
+            secondStringMassive[count] = firstStringMassive[i];
+            count++;
+        }
+    }
+}
 
 void PrintArray(string[] array)
 {
